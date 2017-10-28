43a4661425c3151edbc60402dd0bb48d29ea96fa
Brayden Girard <brayden@daydreamdev.com>
2017-10-28 16:03:32 -0400

    You can now add players

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
+interface Player {  
+  name: string,
+  weight: number,
+  active: boolean
+}
+
+interface Group {  
+  name: string;  
+  players: Player[];  
+}
 
 @Component({
   selector: 'page-home',
@@ -7,7 +18,63 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  group: Group = { name: "", players: [] };
+  weight: number = null;
+  name: string = null;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  addPlayer() {
+    if(this.weight && this.name) {
+      this.group.players.push({ name: this.name, weight: this.weight, active: true })
+      this.weight = null;
+      this.name = null;
+    }
+    else {
+      this.showError('Missing Information', 'You must enter a name and select a weight.')
+    }
+  }
+
+  showError(title: string, text: string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  showTeamPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'GENERATE TEAMS',
+      message: "Your group has " + this.group.players.length + " players.",
+      inputs: [
+        {
+          name: 'numberOfPlayers',
+          placeholder: '# of teams'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            
+          }
+        },
+        {
+          text: 'Generated',
+          handler: data => {
+            this.generateTeams(data.numberOfPlayers);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  generateTeams(numberOfTeams: number) {
 
   }
 
